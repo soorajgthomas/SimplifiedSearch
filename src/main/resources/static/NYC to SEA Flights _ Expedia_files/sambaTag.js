4b9f3f3d95de6acd6f5989c09ac9b24c0104d4de
Shekhar R <a-8177@LE1016.local>
2018-07-10 15:28:08 +0530

    shekhar changes

---
+++ src/main/resources/static/NYC to SEA Flights _ Expedia_files/sambaTag.js
@@ -0,0 +1,51 @@
+
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+    <title>Access Denied</title>
+
+<link rel="stylesheet" type="text/css" media="all" href='../portal/static/176151140/resellers/default/css/general.css'/>
+<!--[if IE 6]>
+<link rel="stylesheet" type="text/css" media="all" href='../portal/static/176151140/resellers/default/css/general_ie6.css'/>
+<![endif]-->
+<!--[if IE 7]>
+<link rel="stylesheet" type="text/css" media="all" href='../portal/static/176151140/resellers/default/css/general_ie7.css'/>
+<![endif]-->
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+</head>
+
+<body id="alert">
+<div id="alertframe">
+        <div id="alertframepad">
+
+            <div id="alertframetop">
+                <span class="mainbox_ul"></span>
+                <span class="mainbox_ur"></span>
+            </div>
+
+            <div id="alertheader">
+                <div id="logo1"></div>
+                <div id="logo2"></div>
+            </div>
+                <!-- alert_icon_text is not used by all brands. Is hidden with css where not used -->
+                <div class="alert_icon_text">Access Denied</div>            
+                <div id="alertbody">
+                    <h1>Access Denied</h1>
+                    <p><p>The https://tag.mtrcs.samba.tv/ has been deemed by your administrator to be unsafe or unsuitable for you to access. The resource has been blocked. No further action is required.</p><p><b>Reason: </b>The category of Entertainment has been blocked by your System Administrator<p></p>
+                    <p></p>
+                </div>
+            <div id="alertframebottom">
+                <span class="mainbox_dl"></span>
+                <span class="mainbox_dr"></span>
+            </div>
+        
+        </div>
+</div>
+</body>
+</html>
---
+++ src/main/resources/static/homepage_files/verify.js
@@ -0,0 +1,12 @@
+var dvObj = $dvbs;function np764531(g,i){function d(){function d(){function f(b,a){b=(i?"dvp_":"")+b;e[b]=a}var e={},a=function(b){for(var a=[],c=0;c<b.length;c+=2)a.push(String.fromCharCode(parseInt(b.charAt(c)+b.charAt(c+1),32)));return a.join("")},h=window[a("3e313m3937313k3f3i")];h&&(a=h[a("3g3c313k363f3i3d")],f("pltfrm",a));(function(){var a=e;e={};dvObj.registerEventCall(g,a,2E3,true)})()}try{d()}catch(f){}}try{dvObj.pubSub.subscribe(dvObj==window.$dv?"ImpressionServed":"BeforeDecisionRender",g,"np764531",d)}catch(f){}}
+;np764531("e891decd4f654085b1053da49e33868f",false);
+
+
+try{__tagObject_callback_89881035812({ImpressionID:"e891decd4f654085b1053da49e33868f", ServerPublicDns:"tps710.doubleverify.com"});}catch(e){}
+try{$dvbs.pubSub.publish('BeforeDecisionRender', "e891decd4f654085b1053da49e33868f");}catch(e){}
+try{__verify_callback_89881035812({
+ResultID:1,
+Passback:"",
+AdWidth:300,
+AdHeight:250});}catch(e){}
+try{$dvbs.pubSub.publish('AfterDecisionRender', "e891decd4f654085b1053da49e33868f");}catch(e){}
